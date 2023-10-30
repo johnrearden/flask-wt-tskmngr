f868e8280d560d60f30eb6b2743dece726785b23
johnrearden <isnocturnal@gmail.com>
2023-10-30 20:04:10 +0000

    Work on edit category lesson

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +6,27 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    categories = Category.query.order_by(Category.category_name).all()
+    print(type(categories))
+    categories = list(categories)
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get('category_name'))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for('categories'))
+    return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    return render_template("edit_category.html", category=category)
